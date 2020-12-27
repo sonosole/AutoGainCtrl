bcdee191147dea561b840e3a2574d4a88a9d11f7
sole <taiyang_zhao@163.com>
2020-12-27 14:39:43 +0800

    offline and online agc

--- src/AGC.jl
+++ src/AGC.jl
@@ -1,33 +1,15 @@
 module AGC
 
+# offline part
 export agc!
 export agc
 
-function agc!(wav::Array, fs::Real=16000.0; maxvalue=0.6)
-    gain   = 1.0
-    winlen = floor(Int, 0.016 * fs)
-    frames = div(length(wav)-winlen, winlen) + 1
-    firstIds = (0:(frames-1)) .* winlen .+ 1       # 帧起始下标
-    lasstIds = firstIds .+ (winlen - 1)            # 帧结束下标
-    maxvalue = 1/(maxvalue + 1e-38)
-    for t = 1:frames
-        index = firstIds[t]:lasstIds[t];
-        frame = wav[index];
-        hmax = maximum(abs.(frame));
-        step = 1.0 - hmax*maxvalue;
-        step = step * abs(step);
-        step = max(-0.6, min(step, 0.0));
-        gain = 0.8*gain + 0.2*(1 + step);
-        wav[index] .*= gain;
-    end
-end
+# online part
+export setagc
+export onlineagc!
+export onlineagc
 
-
-function agc(wav::Array, fs::Real=16000.0; maxvalue=0.6)
-    data = deepcopy(wav)
-    agc!(data, fs; maxvalue = maxvalue)
-    return data
-end
-
+include("offline-agc.jl")
+include("online-agc.jl")
 
 end #module
