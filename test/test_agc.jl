1eb155e3afe8e146d138a247fe7906dbc5acad30
sole <taiyang_zhao@163.com>
2020-12-27 14:41:14 +0800

    Delete test_agc.jl

--- test/test_agc.jl
+++
@@ -1,9 +0,0 @@
-using WAV
-using AGC
-
-wav, fs = wavread("./doc/123.wav")
-agc!(wav,fs,maxvalue=0.3)
-new = agc(wav,fs,maxvalue=0.3)
-
-wavwrite(wav, "./doc/agced1.wav", Fs=fs, nbits=32)
-wavwrite(new, "./doc/agced2.wav", Fs=fs, nbits=32)
